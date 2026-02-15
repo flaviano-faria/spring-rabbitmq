8158b24ed820f29d31e03c4db755cbc482d3e96f
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-14 22:08:20 -0300

    Initial commit

---
+++ src/test/java/com/javabridge/springrabbitmq/SpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabridge.springrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
