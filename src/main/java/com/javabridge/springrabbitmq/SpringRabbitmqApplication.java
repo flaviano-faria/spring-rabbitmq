e96ea886a1352aebf2266799b326240c5b783aa1
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-15 23:30:48 -0300

    Enable Rabbit

--- src/main/java/com/javabridge/springrabbitmq/SpringRabbitmqApplication.java
+++ src/main/java/com/javabridge/springrabbitmq/SpringRabbitmqApplication.java
@@ -1,9 +1,11 @@
 package com.javabridge.springrabbitmq;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class SpringRabbitmqApplication {
 
     public static void main(String[] args) {
