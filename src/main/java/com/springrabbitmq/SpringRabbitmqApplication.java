d057f334fe903d0b1f5c41d8dd4d3565f3c1c8c2
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-16 15:49:50 -0300

    Updating packages

--- src/main/java/com/springrabbitmq/SpringRabbitmqApplication.java
+++ src/main/java/com/springrabbitmq/SpringRabbitmqApplication.java
@@ -1,4 +1,4 @@
-package com.javabridge.springrabbitmq;
+package com.springrabbitmq;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/springrabbitmq/SpringRabbitmqApplicationTests.java
+++ src/test/java/com/springrabbitmq/SpringRabbitmqApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javabridge.springrabbitmq;
+package com.springrabbitmq;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
