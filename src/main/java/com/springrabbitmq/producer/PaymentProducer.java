09bd02d16811250d401cf7a455feb6851d189e72
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-16 16:02:32 -0300

    Creating controller, service and producer

---
+++ src/main/java/com/springrabbitmq/producer/PaymentProducer.java
@@ -0,0 +1,7 @@
+package com.springrabbitmq.producer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentProducer {
+}
---
+++ src/main/java/com/springrabbitmq/service/PaymentService.java
@@ -0,0 +1,7 @@
+package com.springrabbitmq.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+}
