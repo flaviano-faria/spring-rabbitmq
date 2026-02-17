1a04e1afe53de1570fa7cd1765d5d0ff05630fbf
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-17 17:41:36 -0300

    Implementing PaymentProducer

--- src/main/java/com/springrabbitmq/producer/PaymentProducer.java
+++ src/main/java/com/springrabbitmq/producer/PaymentProducer.java
@@ -1,7 +1,21 @@
 package com.springrabbitmq.producer;
 
+import com.springrabbitmq.dto.PaymentDTO;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class PaymentProducer {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public void sendPayment(PaymentDTO paymentDTO) {
+        amqpTemplate.convertAndSend("payment-exchange",
+                "payment-rk",
+                mapper.writeValueAsString(paymentDTO));
+    }
 }
