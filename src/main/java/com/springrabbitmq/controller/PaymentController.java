e4765e14bf4eeee87c9d8331037cd3aff5faf135
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-18 21:06:55 -0300

    Implementing PaymentController.java

--- src/main/java/com/springrabbitmq/controller/PaymentController.java
+++ src/main/java/com/springrabbitmq/controller/PaymentController.java
@@ -1,7 +1,20 @@
 package com.springrabbitmq.controller;
 
+import com.springrabbitmq.dto.PaymentDTO;
+import com.springrabbitmq.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payments")
+    public void sendPayment(@RequestBody PaymentDTO paymentDTO) {
+        paymentService.sendPayment(paymentDTO);
+    }
 }
