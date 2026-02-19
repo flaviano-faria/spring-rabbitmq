4822849920cb5109dea61d389f85933da83a11ec
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-18 21:03:37 -0300

    Implementing PaymentService.java

--- src/main/java/com/springrabbitmq/service/PaymentService.java
+++ src/main/java/com/springrabbitmq/service/PaymentService.java
@@ -1,7 +1,17 @@
 package com.springrabbitmq.service;
 
+import com.springrabbitmq.dto.PaymentDTO;
+import com.springrabbitmq.producer.PaymentProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+    @Autowired
+    private PaymentProducer paymentProducer;
+
+    public void sendPayment(PaymentDTO paymentDTO) {
+        paymentProducer.sendPayment(paymentDTO);
+    }
 }
