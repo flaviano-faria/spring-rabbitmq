8cef6e0d58e2ad2ae37b619f5554e369dd844669
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-19 21:18:44 -0300

    Fixing PaymentProducer

--- src/main/java/com/springrabbitmq/producer/PaymentProducer.java
+++ src/main/java/com/springrabbitmq/producer/PaymentProducer.java
@@ -1,11 +1,10 @@
 package com.springrabbitmq.producer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springrabbitmq.dto.PaymentDTO;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class PaymentProducer {
@@ -15,13 +14,9 @@ public class PaymentProducer {
     private final ObjectMapper mapper = new ObjectMapper();
 
     public void sendPayment(PaymentDTO paymentDTO) {
-        try {
-            String paymentJson = mapper.writeValueAsString(paymentDTO);
-            amqpTemplate.convertAndSend("payment-exchange",
-                    "payment-rk",
-                    paymentJson);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to serialize payment DTO", e);
-        }
+        String paymentJson = mapper.writeValueAsString(paymentDTO);
+        amqpTemplate.convertAndSend("payment-exchange",
+                "payment-rk",
+                paymentJson);
     }
 }
