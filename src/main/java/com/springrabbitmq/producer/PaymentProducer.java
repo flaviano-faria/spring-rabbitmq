7c056f59582278e3403fa79d85504cf825728b37
flaviano-faria <flsilv89@yahoo.com.br>
2026-02-19 21:04:08 -0300

    Creating README.md

--- src/main/java/com/springrabbitmq/producer/PaymentProducer.java
+++ src/main/java/com/springrabbitmq/producer/PaymentProducer.java
@@ -1,21 +1,27 @@
 package com.springrabbitmq.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springrabbitmq.dto.PaymentDTO;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tools.jackson.databind.ObjectMapper;
 
 @Service
 public class PaymentProducer {
 
     @Autowired
     private AmqpTemplate amqpTemplate;
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public void sendPayment(PaymentDTO paymentDTO) {
-        amqpTemplate.convertAndSend("payment-exchange",
-                "payment-rk",
-                mapper.writeValueAsString(paymentDTO));
+        try {
+            String paymentJson = mapper.writeValueAsString(paymentDTO);
+            amqpTemplate.convertAndSend("payment-exchange",
+                    "payment-rk",
+                    paymentJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to serialize payment DTO", e);
+        }
     }
 }
